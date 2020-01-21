a0cf4dbf64e21ec9896ea5f353186fb58ee59934
Nassim <nassim.morouche1@gmail.com>
2020-01-21 17:16:52 +0100

    add

--- DHT-sensor/DHT-sensor.ino
+++ DHT-sensor/DHT-sensor.ino
@@ -6,9 +6,16 @@ DHT dht(DHTPIN, DHTTYPE);
 const int redLed = D1;
 const int greenLed = D5;
 const int yellowLed = D6;
+
 float* temp = new float[10];
 float* median = new float[10];
+
+float* humidity = new float[10];
+float* humidityMedian = new float[10];
+
 float previousMoyenne = 0.0;
+float previousMoyenneHumidity = 0.0;
+
 int count = 0;
 
 void setup() {
@@ -28,16 +35,22 @@ void loop() {
 
   if(count < 10){
     temp[count] = t;
+    humidity[count] = h;
     count += 1;
   } else{
+    // Temperatures gestion
     fillAndSortMedianTab();
     displayValues();
-    checkTendance();
-    previousMoyenne = tempSum();
-    decrementTab(t);
-  }
- 
-
+    
+    //checkTendance();
+    checkHumidityTendance();
+    
+    //previousMoyenne = tempSum();
+    previousMoyenneHumidity = humiditySum();
+    
+    decrementTab(t, h);
+   }
+  
   if (isnan(h) || isnan(t) || isnan(f)) {
     Serial.println("Impossible de lire la sonde DHT!");
     return;
@@ -60,6 +73,23 @@ void checkTendance() {
   }
 }
 
+void checkHumidityTendance() {
+  if(previousMoyenneHumidity < humiditySum()) {
+    digitalWrite(greenLed, HIGH);
+    digitalWrite(redLed, LOW);
+    digitalWrite(yellowLed, LOW);
+  } else if(previousMoyenne > tempSum()) {
+    digitalWrite(redLed, HIGH);
+    digitalWrite(greenLed, LOW);
+    digitalWrite(yellowLed, LOW);
+  } else {
+    digitalWrite(yellowLed, HIGH);
+    digitalWrite(greenLed, LOW);
+    digitalWrite(redLed, LOW);
+  }
+}
+
+
 float tempSum() {
   float tempSum = 0;
     for(int i = 0; i < 10; i++){
@@ -69,11 +99,25 @@ float tempSum() {
     return tempSum = tempSum / 10.0;
 }
 
-void decrementTab(float t) {
+float humiditySum() {
+  float humiditySum = 0;
+    for(int i = 0; i < 10; i++){
+      humiditySum += humidity[i];
+    }
+    // Affichage temperature moyenne
+    return humiditySum = humiditySum / 10.0;
+}
+
+void decrementTab(float t, float h) {
     for(int i = 1; i < 10; i++){
         temp[i-1] = temp[i];
     }
     temp[9] = t;
+
+    for(int i = 1; i < 10; i++){
+        humidity[i-1] = humidity[i];
+    }
+    humidity[9] = h;
 }
 
 void displayValues() {
@@ -83,10 +127,19 @@ void displayValues() {
     Serial.print("\nTempérature median: ");
     Serial.print(getMedianValue());
     Serial.println(" °C");
+
+    Serial.print("Humidité moyenne: ");
+    Serial.print(humiditySum());
+    Serial.println(" %");
+    Serial.print("Humidité median: ");
+    Serial.print(getHumidityMedianValue());
+    Serial.println(" %");
 }
+
 
 void sortMedianTab() {
   int min;
+  int minH;
   for(int i = 0; i < 10; i++){
      min = i;
      for(int j = i + 1; j < 10; j++){
@@ -100,15 +153,35 @@ void sortMedianTab() {
         median[min] = medianVal;
      }
   }
+  for(int i = 0; i < 10; i++){
+     minH = i;
+     for(int j = i + 1; j < 10; j++){
+         if(humidityMedian[j] < humidityMedian[minH]){
+             minH = j;
+         }
+     }
+     if(minH != i){
+        int medianVal = humidityMedian[i];
+        humidityMedian[i] = humidityMedian[minH];
+        humidityMedian[minH] = medianVal;
+     }
+  }
 }
 
 float getMedianValue() {
   return median[4];
 }
 
+float getHumidityMedianValue() {
+  return humidityMedian[4];
+}
+
 void fillAndSortMedianTab() {
   for(int i=0; i<10; i++) {
     median[i] = temp[i];
+  }
+  for(int i=0; i<10; i++) {
+    humidityMedian[i] = humidity[i];
   }
   sortMedianTab();
 }
